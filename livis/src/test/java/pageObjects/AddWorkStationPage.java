33afb699a187611a6e1fb0ab33e423dc11ac62d3
rushikesh007-tech <rushikesh.rakte@lincode.ai>
2024-08-06 15:45:47 +0530

    workstation commit

--- livis/src/test/java/pageObjects/AddWorkStationPage.java
+++ livis/src/test/java/pageObjects/AddWorkStationPage.java
@@ -1,8 +1,11 @@
 package pageObjects;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class AddWorkStationPage extends BasePage{
 
@@ -16,8 +19,12 @@ public class AddWorkStationPage extends BasePage{
 	@FindBy(xpath="//input[@id='workstation_location']") WebElement txtWSLocationname;
 	@FindBy(xpath="(//div[contains(@class,' livis-stepper-button-grid ')])[2]") WebElement btnNext;
 	@FindBy(xpath="//img[@alt='static-station-image']") WebElement imgStaticStation;
-	
-	
+	@FindBy(xpath="(//input[@name='camera.0.camera_name'])[1]") WebElement txtCameraName;
+	@FindBy(id="camera.0.camera_type-id")   WebElement dropdownContainer;;
+	@FindBy(xpath="//ul[@role='listbox']//li[@role='option']") List<WebElement> dropdownOptions;
+	@FindBy(xpath="//input[@name='camera.0.camera_address']") WebElement cameraIpField;
+	@FindBy(xpath="//input[contains(@name,'camera.0.camera_address')]") WebElement  submitButton;
+	@FindBy(xpath="(//button[@id='livis-button-id'])[2]") WebElement btnCreate;
 
 	
 	public void setWorkStationName(String sname) {
@@ -37,5 +44,38 @@ public class AddWorkStationPage extends BasePage{
 		btnNext.click();
 	}
 	
+	public void clickonImgStaticStation() {
+		imgStaticStation.click();
+	}
 	
+	public void setCameraName(String cname) {
+		txtCameraName.sendKeys(cname);		
+	}
+	
+	 public void openDropdown() {
+	        dropdownContainer.click();
+	    }
+	
+	 public void selectDropdownOptionByVisibleText(String visibleText) {
+	        openDropdown();
+	        for (WebElement option : dropdownOptions) {
+	            if (option.getText().equals(visibleText)) {
+	                option.click();
+	                break;
+	            }
+	        }
+	    }
+	 public void clickSubmit() {
+	        submitButton.click();
+	    }
+		
+	 public void enterCameraIp(String ipAddress) {
+		    clickSubmit();
+	        cameraIpField.clear();
+	        cameraIpField.sendKeys(ipAddress);
+	    } 
+	 	
+	 public void clickOnCreateButton() {
+		 btnCreate.click();
+	 }
 }
--- livis/src/test/java/testCases/TC002_AddWorkstationTest.java
+++ livis/src/test/java/testCases/TC002_AddWorkstationTest.java
@@ -2,11 +2,12 @@ package testCases;
 
 import org.testng.annotations.Test;
 
+import dev.failsafe.internal.util.Assert;
 import pageObjects.AddWorkStationPage;
 import pageObjects.HomePage;
 
 public class TC002_AddWorkstationTest extends TC001_LoginTest{
-	@Test(priority=3)
+	@Test(priority=2)
 	public void verify_addWorkStation() {
 		
 		HomePage hp= new HomePage(driver);
@@ -17,6 +18,14 @@ public class TC002_AddWorkstationTest extends TC001_LoginTest{
 		ws.setWSPlantName("PlantB");
 		ws.setWSLocationName("India");
 		ws.clickOnNextButton();
+		ws.clickonImgStaticStation();
+		ws.clickOnNextButton();
+		ws.setCameraName("C1");
+		ws.selectDropdownOptionByVisibleText("USB Cam");	    
+		ws.enterCameraIp("1");		
+		ws.clickOnNextButton();
+		ws.clickOnCreateButton();
+		
 	}
 
 }
