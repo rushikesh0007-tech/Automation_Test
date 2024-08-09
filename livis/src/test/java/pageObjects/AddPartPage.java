ca40383570e23ad3127f9ac27d4508763f7aa2e3
rushikesh0007-tech <rushikesh.rakte@lincode.ai>
2024-08-09 17:53:45 +0530

    Update AddPartPage.java

--- livis/src/test/java/pageObjects/AddPartPage.java
+++ livis/src/test/java/pageObjects/AddPartPage.java
@@ -52,8 +52,11 @@ public class AddPartPage extends BasePage {
 	public void clickoncreateButton() {
 		btnCreate.click();
 	}
-	
+
 	public void clickOnUploadImage() {
+		btnClickonUploadimage.click();
+	}
+	public void clickOnUploadImagetesting() {
 		btnClickonUploadimage.click();
 	}
 }
