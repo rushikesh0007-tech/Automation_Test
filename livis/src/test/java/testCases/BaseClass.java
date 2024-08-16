20bd1568a4cad4575cc2dd94eaba5bb1be5eed63
rushikesh007-tech <rushikesh.rakte@lincode.ai>
2024-08-16 10:33:50 +0530

    initialcommit;

--- livis/src/test/java/testCases/BaseClass.java
+++ livis/src/test/java/testCases/BaseClass.java
@@ -2,10 +2,12 @@ package testCases;
 
 import java.time.Duration;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseClass {
 	
@@ -13,8 +15,10 @@ public class BaseClass {
 	
 	@BeforeClass
 	public void setup() {
+		ChromeOptions chromeoptions= new ChromeOptions();
+		chromeoptions.addArguments("headlessargument");
 		
-		 driver = new ChromeDriver();
+		 driver = new ChromeDriver(chromeoptions);
 		 driver.manage().deleteAllCookies();
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		 driver.get("https://demo.livis.ai/auth/login");
