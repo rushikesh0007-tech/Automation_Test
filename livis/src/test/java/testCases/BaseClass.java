7806973d67d9f7ba993f2a9d0fbbcbbd2945ae9e
rushikesh007-tech <rushikesh.rakte@lincode.ai>
2024-08-16 11:47:34 +0530

    initialcommit

--- livis/src/test/java/testCases/BaseClass.java
+++ livis/src/test/java/testCases/BaseClass.java
@@ -15,9 +15,17 @@ public class BaseClass {
 	
 	@BeforeClass
 	public void setup() {
-		
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Run in headless mode
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
+        options.addArguments("--disable-gpu"); // Disable GPU hardware acceleration
+        options.addArguments("--remote-allow-origins=*"); // Allow all origins for remote access
+        options.addArguments("--window-size=1920x1080"); // Set screen size to avoid issues
 
-		 driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
+
+		// driver = new ChromeDriver();
 		 driver.manage().deleteAllCookies();
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		 driver.get("https://demo.livis.ai/auth/login");
